8942865c8a69ced16257145d17aa3f1afdc899e6
takehara <higo.hh.bb.moccos@gmail.com>
2022-04-19 11:38:40 +0900

    Add: mybag削除(不備あり)

--- dist/items/mybag.php
+++ dist/items/mybag.php
@@ -1,5 +1,26 @@
 <?php include("../parts/after-header.php") ?>
 <?php
+// フラッシュメッセージ
+$flash = isset($_SESSION['flash']) ? $_SESSION['flash'] : array();
+unset($_SESSION['flash']);
+foreach (array('info', 'success', 'warning', 'error') as $key) {
+  if (strlen(@$flash[$key])) {
+?>
+    <div class="js-flash lg:w-1/3 sm:w-1/2 absolute top-32 right-5">
+      <div class="alert <?php echo 'alert-' . $key  ?> shadow-lg opacity-90">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" class="dismiss cursor-pointer stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+          <span><?php echo $flash[$key] ?></span>
+        </div>
+      </div>
+    </div>
+<?php
+  }
+}
+?>
+<?php
 if (isset($_POST["item_id"])) {
   if (isset($_SESSION["bag"])) {
     foreach ((array)$_SESSION["bag"] as $key => $item) {
@@ -14,7 +35,6 @@ try {
   $total = 0;
   $total_price = 0;
   require_once "../common/basedb.php";
-
   if (isset($_SESSION["bag"])) {
     echo <<<EOF
       <div class="body-bag">
@@ -54,7 +74,7 @@ try {
           <!-- Item -->
           <div class="item">
             <div class="buttons">
-              <span class="delete-btn"><i class="fas fa-trash-alt"></i></span>
+              <a href="./bagdestroy.php"><span class="delete-btn"><i class="fas fa-trash-alt"></i></span></a>
               <!-- <span class="like-btn"><i class='far fa-heart'></i></span> -->
             </div>
       
