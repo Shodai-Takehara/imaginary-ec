2d7ebdff25ef4bb5214488ef851c1e106087afd2
takehara <higo.hh.bb.moccos@gmail.com>
2022-04-20 11:11:12 +0900

    Fix: DB情報更新

--- dist/common/basedb.php
+++ dist/common/basedb.php
@@ -3,8 +3,8 @@
 // $password = 'pass1';
 $user = 'b2022';
 $password = 'dB4bApUK';
-$dbName = 'ec';
-// $dbName = 'b202211';
+// $dbName = 'ec';
+$dbName = 'b202211';
 $host = 'localhost:3306';
 $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
 $pdo = new PDO($dsn, $user, $password);
